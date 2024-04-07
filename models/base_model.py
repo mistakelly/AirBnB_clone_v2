c25e4fd2167c54a108df47adb7bd71e76f99f77f
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-07 23:52:10 +0100

    added back the new method to the init method

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            # models.storage.new(self)
 
     def __str__(self) -> str:
         """
@@ -58,8 +58,8 @@ class BaseModel:
             save method
         """
         self.updated_at = datetime.now()
-        models.storage.save()
         models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self) -> dict:
         """
