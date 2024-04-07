cd2e870ae31cb07485980736591957a7bc499fbb
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-08 00:05:41 +0100

    added attributes

--- models/base_model.py
+++ models/base_model.py
@@ -34,13 +34,18 @@ class BaseModel:
                     value = datetime.strptime(value, self.TIME_FORMAT)
                 if key != '__class__':
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
 
         else:
             import models
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # models.storage.new(self)
 
     def __str__(self) -> str:
         """
