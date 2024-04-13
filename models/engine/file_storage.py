578e2b1e81cfb10299385ddb040bf939f670a5a8
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-13 16:51:16 +0100

    updated_reviews

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
-#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
-"""
 #!/usr/bin/python3
+"""
+#!/Users/mistarkelly/vagrant_project/My-Projects/ALX-ONLY/AirBnB_clone_v2/.venv/bin/python3
     responsible for persisting object state in file_db.
 """
 import json
@@ -119,6 +119,3 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             del self.__objects[key]
-
-    def reviews(self):
-        pass
