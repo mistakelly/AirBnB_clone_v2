a90376e0cbb189b2c06f8674e379dd9bcd93fda5
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-27 23:00:35 +0100

    made pycodstyle complaince

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application 
+
+"""Script that starts a Flask web application
 listening on address 0.0.0.0 port 5000
 """
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
